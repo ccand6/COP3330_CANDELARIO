9f24f9d256198fe11665fc50b7bdf7050eb477c0
ccand6 <70596345+ccand6@users.noreply.github.com>
2020-09-28 19:56:42 -0400

    Basic Program Functions Set
    -Program can now print BMI based off of user input, and list what category it is under.
    -Program still needs Y/N input to continue adding user BMIs.
    -Program still needs BMI Statistics to display.

--- CANDELARIO_p2/src/App.java
+++ CANDELARIO_p2/src/App.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App {
 
@@ -18,6 +19,7 @@ public class App {
         displayBmiStatistics(bmiData);
     }
 
+
     public static double getUserHeight() {
         Scanner Input = new Scanner(System.in);
         System.out.print("What is your height in inches?: ");
@@ -32,18 +34,31 @@ public class App {
 
     public static double getUserWeight() {
         Scanner Input = new Scanner(System.in);
-        System.out.print("What is your height in inches?: ");
+        System.out.print("What is your weight in inches?: ");
         double InputW = Input.nextDouble();
 
         while(InputW <= 0) {
-            System.out.print("Please input a height greater than 0 to proceed.: ");
+            System.out.print("Please input a weight greater than 0 to proceed.: ");
             InputW = Input.nextDouble();
         }
         return InputW;
     }
 
-    public static void displayBmiInfo() {
-        System.out.print("Your BMI is:" + bmi);
+    public static boolean moreInput() {
+        //Scanner Input = new Scanner(System.in);
+        //System.out.print("Would you like to add another user's BMI?: Y/N");
+
+        return true;
+    }
+
+    public static void displayBmiInfo(BodyMassIndex bmi) {
+
+        System.out.print("Your BMI is: " + String.format("%.2f", BodyMassIndex.bmi));
+        System.out.print("\nYour BMI is in the category of: " + BodyMassIndex.BMICategory + '\n');
+    }
+
+    public static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData) {
+
     }
 }
 
--- CANDELARIO_p2/src/BodyMassIndex.java
+++ CANDELARIO_p2/src/BodyMassIndex.java
@@ -1,8 +1,25 @@
 public class BodyMassIndex {
-    double pounds = App.weight;
 
-    public static void bmi() {
-        703 * pounds / inches^2
+    public static double bmi;
+    public static String BMICategory;
 
+    public BodyMassIndex(double height, double weight) {
+        bmi = 703 * weight / (Math.pow(height,2));
+        BodyMassCategory();
     }
+
+    public static void BodyMassCategory() {
+        int Category = 0;
+        if(bmi < 18.5) Category = 1;
+        if(bmi >= 18.5 && bmi < 25) Category = 2;
+        if(bmi >= 25 && bmi < 30) Category = 3;
+        if(bmi >= 30) Category = 4;
+
+        if(Category == 1) BMICategory = "Underweight";
+        if(Category == 2) BMICategory = "Normal Weight";
+        if(Category == 3) BMICategory = "Overweight";
+        if(Category == 4) BMICategory = "Obese";
+    }
+
+
 }
